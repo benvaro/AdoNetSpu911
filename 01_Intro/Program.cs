0c2e5e0dcc48502d5f8b1aa24ed209a93e9a5064
benvaro <benvaro91@gmail.com>
2021-02-13 13:21:06 +0200

    Add project files.

---
+++ 01_Intro/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _01_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;
+                                    Initial Catalog=master;
+                                    Integrated Security=True;";
+
+            var connection = new SqlConnection(connectionString);
+            connection.StateChange += Connection_StateChange;
+            //connection.Open();
+            //Console.WriteLine("Connection: State: " + connection.State);
+            //Console.WriteLine("Connection: Database: " + connection.Database);
+            //Console.WriteLine("Connection: Server: " + connection.DataSource);
+            //connection.Close();
+
+            using (connection)
+            {
+                connection.Open();
+                #region Create database
+                //var commandText = "CREATE DATABASE Demo";
+                //var command = new SqlCommand(commandText, connection);
+                //var result = command.ExecuteNonQuery(); // CREATE INSERT DELETE UPDATE
+                //Console.WriteLine("Result create: " + result);
+                #endregion
+
+                connection.ChangeDatabase("Demo");
+                #region Create table
+                //var commandText = "CREATE TABLE Car(" +
+                //    "Id INT IDENTITY PRIMARY KEY," +
+                //    "Brand nvarchar(15) NOT NULL," +
+                //    "Model nvarchar(10) NOT NULL)";
+
+                //var command = new SqlCommand(commandText, connection);
+                //command.ExecuteNonQuery(); 
+                #endregion
+
+                #region Insert data
+                //var commandText = "INSERT INTO Car VALUES ('Audi', 'R8')," +
+                //    "('Mazda', 'CX-5')";
+
+                //var command = new SqlCommand(commandText, connection);
+                //var result = command.ExecuteNonQuery();
+                //Console.WriteLine("Inserted: " + result);
+                #endregion
+                //command.ExecuteScalar(); // min, count, max, sum, avg
+
+                #region Select from table
+                var commandText = "SELECT * FROM Car; select * from Car;";
+                var command = new SqlCommand(commandText, connection);
+                var flag = true;
+                using (var sqlReader = command.ExecuteReader())
+                {
+
+                    do
+                    {
+                        for (int i = 0; i < sqlReader.FieldCount; i++)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write($"{sqlReader.GetName(i),-15}");
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine();
+                        while (sqlReader.Read())
+                        {
+                            if (flag)
+                            {
+
+                                flag = false;
+                            }
+
+                            for (int i = 0; i < sqlReader.FieldCount; i++)
+                            {
+                                Console.Write($"{sqlReader.GetValue(i),-15}");
+                            }
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine();
+                    }
+                    while (sqlReader.NextResult());
+                }
+
+                #endregion
+                //command.ExecuteReader(); // SELECT
+            }
+        }
+
+        private static void Connection_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            var obj = sender as SqlConnection;
+            Console.WriteLine($"State changed: {obj.State}");
+        }
+    }
+}
---
+++ 02_ConnectedMode/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace _02_ConnectedMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["local"].ConnectionString;
+
+            #region Connection string builder
+            //var connectionStringBuilder = new SqlConnectionStringBuilder();
+            //connectionStringBuilder.UserID = "test";
+            //connectionStringBuilder.Password = "1";
+            //connectionStringBuilder.DataSource = "194.44.93.225"; 
+            #endregion
+
+            //var connection = new SqlConnection(connectionStringBuilder.ConnectionString);
+            var connection = new SqlConnection(connectionString);
+
+            using (connection)
+            {
+                connection.Open();
+                ReadData(connection);
+                Console.WriteLine("Enter id: ");
+                var id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter brand: ");
+                var brand = Console.ReadLine();
+
+                var brandParameter = new SqlParameter("@p1", System.Data.SqlDbType.NVarChar, 15);
+                brandParameter.Value = brand;
+
+                var commandText = $"Update Car set Brand=@p1 where Id=@p2";
+                var command = new SqlCommand(commandText, connection);
+                command.Parameters.Add(brandParameter);
+                command.Parameters.AddWithValue("@p2", id);
+                command.ExecuteNonQuery();
+                Console.WriteLine();
+                ReadData(connection);
+            }
+        }
+
+        private static void ReadData(SqlConnection connection)
+        {
+            var commandText = $"SELECT * FROM Car";
+            var command = new SqlCommand(commandText, connection);
+            var flag = true;
+            using (var sqlReader = command.ExecuteReader())
+            {
+                for (int i = 0; i < sqlReader.FieldCount; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{sqlReader.GetName(i),-15}");
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+                while (sqlReader.Read())
+                {
+                    if (flag)
+                    {
+
+                        flag = false;
+                    }
+
+                    for (int i = 0; i < sqlReader.FieldCount; i++)
+                    {
+                        Console.Write($"{sqlReader.GetValue(i),-15}");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
